39eb03b5b45184259d692ec559b0092d0ea1a65b
bitimovtm-sketch <bitimov.tm@gmail.com>
2026-06-25 14:54:28 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import logging
 from flask import Flask
 from vin_sync import vin_bp
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+)
 
 app = Flask(__name__)
 app.register_blueprint(vin_bp)
